fc2aa52adfb1ab6a2c2f74488521a7047637f133
zzzzmayday <1099750503@qq.com>
2017-05-08 11:15:24 +0800

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,16 @@
+var $ = Framework7.$;
+var myApp = new Framework7();
+var mainView = myApp.addView('.view-main');
+$('.js-list').on('click',function(){
+	if($('input[name="username"]').val()=='zz' && $('input[name="password"]').val()=='123'){
+		myApp.closeModal();
+		mainView.router.reloadPage('index.html');
+	} else {
+		myApp.alert('wrong!','attention');
+	}
+});
+$(document).on('pageInit',function(){
+	var mySwiper = myApp.swiper('.swiper-container', {
+			pagination: '.swiper-pagination'
+		});
+});
