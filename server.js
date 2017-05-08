78fb5829cfd35d45b827d8d13419e8d96e0b7d0d
zzzzmayday <1099750503@qq.com>
2017-05-08 18:55:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
-'use strict';
-
 var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var path = require('path');
-var root = path.resolve('.');
+var url=require('url');
+var fs=require('fs');
+var path=require('path');
 
-var server = http.createServer((request, response) => {
-    var pathName = url.parse(request.url).pathname;
-    var filePath = path.join(root, pathName);
-    var fileName = path.basename(filePath);
-    console.log(request.method + ': ' + request.url);
-//     fs.createReadStream(filePath).pipe(response);
+var server = http.createServer(function (request, response) {
+    var pathname = url.parse(request.url).pathname;
+    var realPath = path.join("assets", pathname);
+    //console.log(realPath);
+    var ext = path.extname(realPath);
+    ext = ext ? ext.slice(1) : 'unknown';
+    fs.exists(realPath, function (exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
+
+            response.write("This request URL " + pathname + " was not found on this server.");
+            response.end();
+        } else {
+            fs.readFile(realPath, "binary", function (err, file) {
+                if (err) {
+                    response.writeHead(500, {
+                        'Content-Type': 'text/plain'
+                    });
+                    response.end(err);
+                } else {
+                    var contentType = "text/plain";
+                    response.writeHead(200, {
+                        'Content-Type': contentType
+                    });
+                    response.write(file, "binary");
+                    response.end();
+                }
+            });
+        }
+    });
 });
 
 var serverPort = process.env.PORT || 5000;
