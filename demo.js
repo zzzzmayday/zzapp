7d0379db87bbfd4ba12d2eae56b505c13a736973
zzzz <15257173387@163.com>
2017-06-29 17:43:55 +0800

    06/29

--- demo.js
+++ demo.js
@@ -314,124 +314,124 @@ $('.scroll-part').on('scroll',function(){
 });
 $(document).on('click','.js-activity-setting',function(){
 	var buttons1 = [
-        {
-            text: 'Show All Activity',
-        },
-        {
-            text: 'Show Only Comments',
-        }
-    ];
-    var buttons2 = [
-        {
-            text: 'Cancel',
-            bold: true
-        }
-    ];
-    var groups = [buttons1, buttons2];
-    myApp.actions(groups);
+	{
+		text: 'Show All Activity',
+	},
+	{
+		text: 'Show Only Comments',
+	}
+	];
+	var buttons2 = [
+	{
+		text: 'Cancel',
+		bold: true
+	}
+	];
+	var groups = [buttons1, buttons2];
+	myApp.actions(groups);
 });
 $(document).on('click','.js-add-image',function(){
 	var buttons1 = [
-		{
-			text: '上传附件',
-			label: true
-		},
-        {
-            text: '使用最近一张图片',
-        },
-        {
-            text: '拍照',
-        },
-        {
-            text: '从资料库选择',
-        }
-    ];
-    var buttons2 = [
-        {
-            text: 'Cancel',
-            bold: true
-        }
-    ];
-    var groups = [buttons1, buttons2];
-    myApp.actions(groups);
+	{
+		text: '上传附件',
+		label: true
+	},
+	{
+		text: '使用最近一张图片',
+	},
+	{
+		text: '拍照',
+	},
+	{
+		text: '从资料库选择',
+	}
+	];
+	var buttons2 = [
+	{
+		text: 'Cancel',
+		bold: true
+	}
+	];
+	var groups = [buttons1, buttons2];
+	myApp.actions(groups);
 });
 $(document).on('click','.js-add-comment',function(){
 	var buttons1 = [
-		{
-			text: '<img class="add-member-icon add-comment-icon" src="./image/images/add-member.png"></img>Members',
-			color: 'black',
-			bold: true
-		},
-        {
-            text: '<img class="add-tag-icon add-comment-icon" src="./image/images/add-tag.png"></img>Labels',
-			color: 'black',
-			bold: true
-        },
-        {
-            text: '<img class="add-time-icon add-comment-icon" src="./image/images/add-time.png"></img>Due Date',
-			color: 'black',
-			bold: true
-        },
-        {
-            text: '<img class="add-attachment-icon add-comment-icon" src="./image/images/add-attachment.png"></img>Attachment',
-			color: 'black',
-			bold: true
-        },
-        {
-            text: '<img class="add-list-icon add-comment-icon" src="./image/images/add-list.png"></img>Checklist',
-			color: 'black',
-			bold: true
-        }
-    ];
-    var buttons2 = [
-        {
-            text: '<img class="add-cancel-icon add-comment-icon" src="./image/images/add-cancel.png"></img>Cancel',
-			color: 'red',
-			bold: true
-        }
-    ];
-    var groups = [buttons1, buttons2];
-    myApp.actions(groups);
-    $('.actions-modal').addClass('add-comment-action');
+	{
+		text: '<img class="add-member-icon add-comment-icon" src="./image/images/add-member.png"></img>Members',
+		color: 'black',
+		bold: true
+	},
+	{
+		text: '<img class="add-tag-icon add-comment-icon" src="./image/images/add-tag.png"></img>Labels',
+		color: 'black',
+		bold: true
+	},
+	{
+		text: '<img class="add-time-icon add-comment-icon" src="./image/images/add-time.png"></img>Due Date',
+		color: 'black',
+		bold: true
+	},
+	{
+		text: '<img class="add-attachment-icon add-comment-icon" src="./image/images/add-attachment.png"></img>Attachment',
+		color: 'black',
+		bold: true
+	},
+	{
+		text: '<img class="add-list-icon add-comment-icon" src="./image/images/add-list.png"></img>Checklist',
+		color: 'black',
+		bold: true
+	}
+	];
+	var buttons2 = [
+	{
+		text: '<img class="add-cancel-icon add-comment-icon" src="./image/images/add-cancel.png"></img>Cancel',
+		color: 'red',
+		bold: true
+	}
+	];
+	var groups = [buttons1, buttons2];
+	myApp.actions(groups);
+	$('.actions-modal').addClass('add-comment-action');
 });
 $(document).on('click','.js-card-ellipsis-icon',function(){
 	var cardName = $('.card-title').text();
 	var buttons1 = [
-		{
-			text: cardName,
-			label: true
-		},
-        {
-            text: 'Move Card...',
-        },
-        {
-            text: 'Duplicate Card',
-        },
-        {
-            text: 'Copy Link',
-        },
-        {
-            text: 'Subsribe',
-        },
-        {
-            text: 'Archive Card',
-        },
-        {
-            text: 'Send Feedback...',
-        },
-        {
-            text: 'Delete Card',
-            color: 'red'
-        }
-    ];
-    var buttons2 = [
-        {
-            text: 'Cancel',
-            bold: true
-        }
-    ];
-    var groups = [buttons1, buttons2];
-    myApp.actions(groups);
+	{
+		text: cardName,
+		label: true
+	},
+	{
+		text: 'Move Card...',
+	},
+	{
+		text: 'Duplicate Card',
+	},
+	{
+		text: 'Copy Link',
+	},
+	{
+		text: 'Subsribe',
+	},
+	{
+		text: 'Archive Card',
+	},
+	{
+		text: 'Send Feedback...',
+	},
+	{
+		text: 'Delete Card',
+		color: 'red'
+	}
+	];
+	var buttons2 = [
+	{
+		text: 'Cancel',
+		bold: true
+	}
+	];
+	var groups = [buttons1, buttons2];
+	myApp.actions(groups);
 });
 $(document).on('touchstart','.actions-modal-button',function(){
 	$(this).addClass('bg-gray');
@@ -452,3 +452,124 @@ function calHeight(index,flag){
 	}
 	swiperContent[index].style.height = swiperContent.find('.js-card-content')[index].offsetHeight+swiperContent.find('.card-header')[index].offsetHeight+swiperContent.find('.card-footer')[index].offsetHeight+'px';
 }
+
+var drag = {
+	bindDragEvent: function (isF) {
+            var father = $("card-content-inner");//父容器
+            var btns = father.find("public-drag-btn");//事件源对象
+            var items = father.find("card");//拖拽目标
+
+            for (var i = 0; i < items.length; i++) {
+                items[i].index = i;//给拖拽目标加index属性
+                if (isF) {//是否是第一次绑定
+                	drag.bindTouchEvent(btns[i], items[i], items);
+                }else{
+                    if (items[i].getAttribute("data-isBind") == "yes") {//如果是需要绑定的再绑定事件
+                    	items[i].removeAttribute("data-isBind");
+                    	drag.bindTouchEvent(btns[i], items[i], items);
+                    }
+                }
+            }
+
+            /*
+            * 1.循环绑定事件
+            * 2.用分支绑定是为了避免重复绑定事件 造成重复调用的BUG
+            * */
+        },
+        bindTouchEvent: function (ele, target, items) {
+            var dragObj = target;//拖拽目标
+            var btn = ele;//事件源对象
+            var canMove = true;
+            var startY, direction;
+
+            btn.addEventListener("touchstart", touchStart, false);
+
+            function touchStart(event) {
+            	dragObj.className = "item draging";
+                /*
+                 * .draging
+                * 当手指触摸时给个 拖拽中的效果
+                * transform:scale(0.8,0.8);
+                * z-index: 999;
+                * transition: all 1s;
+                * */
+
+                startY = event.touches[0].clientY;//记录初次触摸位置 用以判断移动方向
+                dragObj.addEventListener("touchmove", touchMove, false);//给拖拽目标绑定touchmove 因为要操作拖拽目标
+            }
+
+
+            function touchMove(event) {
+                var i = dragObj.index;//用到index
+                if (event.touches[0].clientY < startY && canMove) {
+                    /*移动<开始  就是向上移动
+                     *canMove是在完成效果后禁止再向下移动
+                     * 也就是说用户移动一次就确定方向了 禁止先 拖到上方，在拖到下方
+                     * */
+                    if (i != 0) {//如果不是第一个项
+                        direction = 1;//有效1 up
+                        dragObj.className = "item draging up";
+                        /*
+                        * .up
+                        *  transform: translate3d(20px,-100%,10px)
+                        *  给一个自身向上移动百分百
+                        * */
+                        items[i - 1].className = "item draging down";
+                        /*当前被拖拽目标的上一个项 给一个向下移动的动效
+                        * .down
+                        *  transform: translate3d(20px,100%,10px)
+                        * */
+                        canMove = false;
+                    }
+                }
+                else if (event.touches[0].clientY > startY && canMove) {
+                    direction = 0;//向下移动
+                    dragObj.className = "item draging down";
+                    items[i + 1].className = "item draging up";
+                    canMove = false;
+                }
+                dragObj.addEventListener("touchend", touchEnd, false);
+            }
+
+            function touchEnd() {
+            	var father = document.getElementById("public_theme_list");
+                var clone = null;//克隆空对象
+                var i = dragObj.index;
+                var newItems;
+                dragObj.className = "item";//清除拖拽目标移动效果
+                clone = dragObj.cloneNode(true);//克隆移动目标
+                if (direction) {
+                    //向上
+                    items[i - 1].className = "item";//当前被拖拽目标的上一个项 给一个向下移动的动效
+                    clone.index = i - 1;//给克隆对象index 为当前上一个项index
+                    items[i - 1].index = i + 1;//拖拽对象前上一个项index+1
+
+                    clone.setAttribute("data-isBind","yes");//dom 不能克隆事件 所以在递归的时候要给克隆绑定事件
+
+                    father.insertBefore(clone,items[i - 1]);
+                    /*将克隆对象插入当前拖拽目标 前一项之前*/
+                    newItems = father.getElementsByClassName("item");
+                    /*因为新增了项 所以获取新项数组*/
+                    father.removeChild(newItems[i + 1]);
+                    /*删除当前拖拽项*/
+                }
+                else {
+                	items[i + 1].className = "item";
+                	clone.index = i + 1;
+                	clone.setAttribute("data-index", i + 1);
+                	items[i + 1].index = i;
+                	items[i + 1].setAttribute("data-index", i);
+
+                	clone.setAttribute("data-isBind","yes");
+
+                	father.insertBefore(clone, items[i + 2]);
+                	newItems = father.getElementsByClassName("item");
+                	father.removeChild(newItems[i]);
+                }
+
+                drag.bindDragEvent(false);//递归调用
+            }
+        }
+    };
+
+    drag.bindDragEvent(true);
